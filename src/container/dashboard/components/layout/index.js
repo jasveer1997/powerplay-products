2bf29f6b0d2122e07095e15c228bda16722a07c5
jasveer <jasveer.singh@mindtickle.com>
2024-01-06 15:59:28 +0530

    feat: added cart and product details redirection header

---
+++ src/container/dashboard/components/layout/index.js
@@ -0,0 +1,87 @@
+import { Card, Row, Col, Space, Button } from 'antd';
+import {Link, useNavigate} from 'react-router-dom';
+import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
+import {useState} from "react";
+
+const ProductsLayout = ({ products, setItemCount, itemCount }) => {
+    const [expandedDescription, setExpandedDescription] = useState({});
+    const navigate = useNavigate();
+    const toggleDescription = (index) => {
+        setExpandedDescription((prevExpanded) => ({
+            ...prevExpanded,
+            [index]: !prevExpanded[index],
+        }));
+    };
+
+    // no limit on addition for now.
+    const incrementItem = (index) => {
+        setItemCount((prevCount) => ({
+            ...prevCount,
+            [index]: (prevCount[index] || 0) + 1,
+        }));
+    };
+
+    const decrementItem = (index) => {
+        setItemCount((prevCount) => ({
+            ...prevCount,
+            [index]: (prevCount[index] || 0) - 1,
+        }));
+    };
+
+    return (
+        <Row gutter={[60, 60]}>
+            {products?.map(({ title, description, id, image }, index) => (
+                <Col key={title} xs={24} sm={12} md={8} lg={6}>
+                    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                        <Card
+                            hoverable
+                            style={{
+                                width: '100%',
+                                flex: '1 1 auto',
+                                border: 'none',
+                            }}
+                            bodyStyle={{ minHeight: '200px' }}
+                            actions={[
+                                <div key="details">
+                                <Button onClick={() => navigate(`/product/${id}`)}>
+                                    Details
+                                </Button>
+                                </div>,
+                                <div key="actions" style={{ display: 'flex', alignItems: 'center' }}>
+                                    <Button
+                                        type="text"
+                                        icon={<MinusOutlined />}
+                                        onClick={() => decrementItem(index)}
+                                        disabled={!(itemCount[index] > 0)}
+                                    />
+                                    {itemCount[index] !== 0 && <div style={{ lineHeight: '32px', margin: '0 8px' }}>{itemCount[index]}</div>}
+                                    <Button type="text" icon={<PlusOutlined />} onClick={() => incrementItem(index)} />
+                                </div>
+                            ]}
+                            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                        >
+                            <Card.Meta
+                                title={title}
+                                description={
+                                    expandedDescription[index] ? description : `${description.slice(0, 100)}...`
+                                }
+                            />
+                            {description.length > 100 && (
+                                <div
+                                    onClick={() => toggleDescription(index)}
+                                    style={{ color: '#1890ff', cursor: 'pointer' }}
+                                >
+                                    {expandedDescription[index] ? 'Show Less' : 'Show More'}
+                                </div>
+                            )}
+                        </Card>
+                    </div>
+                </Col>
+            ))}
+        </Row>
+    );
+};
+
+export default ProductsLayout;
+
+// Todo: Height adjustment upon using actual img.
--- src/container/dashboard/index.js
+++ src/container/dashboard/index.js
@@ -1,23 +1,32 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import { Logout } from '../auth';
-import {Card, Layout, Menu, Skeleton} from 'antd';
+import {Badge, Card, Layout, Menu, Skeleton} from 'antd';
 import {
     AppstoreOutlined,
     TrademarkOutlined,
+    ShoppingCartOutlined,
 } from '@ant-design/icons';
 import { DashboardHeader } from "./components/header";
 import BackButton from "../../components/backButton";
 import container from "./container";
+import ProductsLayout from "./components/layout";
+import {Link} from "react-router-dom";
 const { Header, Content, Footer, Sider } = Layout;
 const { Meta } = Card;
 // import Header from './components/header';
 // import Layout from './components/layout';
 
 const Dashboard = ({ userDetails, fetchProducts, ...rest }) => {
+
+    const [itemCount, setItemCount] = useState({});
+    const totalItems = Object.values(itemCount).reduce((acc, singleItemCount) => {
+        acc += singleItemCount;
+        return acc;
+    }, 0);
     const { products: productsData = {} } = rest;
 
     useEffect(() => {
-        if (!productsData.loaded) {
+        if (!productsData.loaded && !productsData.isLoading) {
             fetchProducts();
         }
     }, [productsData.loaded]);
@@ -72,6 +81,23 @@ const Dashboard = ({ userDetails, fetchProducts, ...rest }) => {
 
                     <div style={{ display: "flex", alignItems: 'center', height: '100%', paddingTop: '-10px' }}>
                         <BackButton />
+                        <div style={{ flex: 1 }} />
+                        {totalItems > 0 && (
+                            <div style={{ marginRight: '24px', marginTop: '10px' }}>
+                                <Badge count={totalItems}>
+                                    <Link to="/cart">
+                                        <ShoppingCartOutlined style={{ fontSize: '24px' }} />
+                                    </Link>
+                                </Badge>
+                            </div>
+                        )}
+                        {!totalItems && (
+                            <div style={{ marginRight: '24px', marginTop: '10px' }}>
+                                <Link to="/cart">
+                                    <ShoppingCartOutlined style={{ fontSize: '24px' }} />
+                                </Link>
+                            </div>
+                        )}
                         <Logout setCurrentUserState={rest.setCurrentUserState}/>
                     </div>
                 </Header>
@@ -81,19 +107,7 @@ const Dashboard = ({ userDetails, fetchProducts, ...rest }) => {
                         overflow: 'initial',
                     }}
                 >
-                    {productsData.isLoading ? <Skeleton active /> :
-                        productsData.products?.map(({ title, description }) => (
-                                <Card
-                                    hoverable
-                                    style={{
-                                        width: 240,
-                                    }}
-                                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                                >
-                                    <Meta title={title} description={description} />
-                                </Card>
-                            ))
-                    }
+                    {productsData.isLoading ? <Skeleton active /> : <ProductsLayout setItemCount={setItemCount} itemCount={itemCount} products={productsData.products}/>}
                 </Content>
                 <Footer
                     style={{
@@ -112,4 +126,4 @@ export default container(Dashboard);
 
 // Todo: Add Carousel of 3-4 images
 // Todo: Convert to skeleton list
-
+// Todo: Add filters, Search, sort
